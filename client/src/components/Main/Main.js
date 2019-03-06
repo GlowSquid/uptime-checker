25ff34287db4cb913ba567aabee353cad456e8d2
glowsquid <koz@glowsquid.com>
2019-03-06 23:52:42 +0100

    it works

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,19 +1,64 @@
-import React, { Component } from 'react';
-import { exec } from 'child_process';
+import React, { Component } from "react";
+// import { exec } from "child_process";
 
-import './Main.css';
+// import jimmy from "../../curl.sh"
+
+import "./Main.css";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      curlOutput: 'magic'
-      // curlThis: ''
+      curlOutput: "",
+      curlThis: "",
+      output: "",
+      cn: "banana"
     };
 
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
+
+  // componentDidMount() {
+  //   this.fetchCurl();
+  // }
+
+  // componentWillUnmount() {
+  //   clearTimeout(this.timer);
+  // }
+
+  fetchCurl = curlThis => {
+    fetch("http://localhost:5002/curl")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ cn: "output__test", curlOutput: json.output });
+
+        // if (json.output === "22") {
+        //   this.setState({
+        //     cn: "output__error",
+        //     curlOutput: "Yup, it's dead. Error: 502 Bad Gateway"
+        //   });
+        // } else if (json.output === "1)") {
+        //   this.setState({
+        //     cn: "output__error",
+        //     curlOutput: "This is not a valid URL"
+        //   });
+        // } else if (json.output === "6)") {
+        //   this.setState({
+        //     cn: "output__error",
+        //     curlOutput: "Yup, it's dead. Error: Could not resolve host"
+        //   });
+        // } else if (json.output === "7)") {
+        //   this.setState({
+        //     cn: "output__error",
+        //     curlOutput: "Failed to connect. Are you even online?"
+        //   });
+        // } else {
+        //   this.setState({ cn: "output__success", curlOutput: json.output });
+        // }
+      })
+      .catch(error => console.log("error: ", error));
+  };
 
   onChange(e) {
     this.setState({ curlThis: e.target.value });
@@ -21,22 +66,8 @@ class Main extends Component {
 
   onSubmit(e) {
     e.preventDefault();
-    this.setState({ curlOutput: this.state.curlThis });
+    this.fetchCurl();
   }
-
-  curly = _ => {
-    exec(
-      `curl -sSf ${this.state.curlThis}`,
-      (err, stdout, stderr, exitreason, exitcode) => {
-        console.log(stdout);
-        process.stdout.write(stdout);
-        process.stderr.write(stderr);
-        if (err) {
-          console.log(stderr.slice(7, 9));
-        }
-      }
-    );
-  };
 
   render() {
     return (
@@ -53,10 +84,12 @@ class Main extends Component {
             onChange={this.onChange}
           />
           <button className="submit" id="btn">
-            Check
+            Submit
           </button>
         </form>
-        <div>{this.state.curlOutput}</div>
+
+        <h3 className={this.state.cn}>{this.state.curlOutput}</h3>
+
         <label>
           Check to see whether a website is working by pasting its URL!
         </label>
--- server/server.js
+++ server/server.js
@@ -1,29 +1,56 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+
+const mongoose = require("mongoose");
+const { exec } = require("child_process");
+const { join } = require("path");
+
 // const bodyParser = require('body-parser');
 
 const app = express();
+app.use(express.static(join(__dirname, "bin")));
+
+app.use(cors({ origin: "http://localhost:3000" }));
+
+app.get("/curl", (req, res, curlThis) => {
+  exec(
+    join(__dirname, "bin", "curl.sh https://glowsquid.com/ipp/"),
+    (err, stdout, stderr) => {
+      if (stderr === null || stderr === "") {
+        return res
+          .status(200)
+          .json({ output: "It's still alive!", error: null });
+      } else if (stdout === null || stdout === "") {
+        // return res.status(200).json({ output: stderr.slice(7, 9) });
+        // console.log("curlThis", curlThis);
+        return res.status(200).json({ output: stderr });
+      } else {
+        return res.status(400).json({ output: null, error: err.message });
+      }
+    }
+  );
+});
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'POST, GET, OPTIONS, PUT, DELETE'
+    "Access-Control-Allow-Methods",
+    "POST, GET, OPTIONS, PUT, DELETE"
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   } else {
-    res.send('kek');
+    res.send("kek");
   }
   next();
 });
 
 const port = process.env.PORT || 5002;
 
-const db = require('./dbconfig').mongoURI;
+const db = require("./dbconfig").mongoURI;
 
 mongoose.connect(db, { useNewUrlParser: true }).then(() => {
-  console.log('db up');
+  console.log("db up");
   app.listen(port);
 });
